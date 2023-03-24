fe23bfd32ffb6260a417a920b90cbd7aaec1e5fe
irvantaufik28 <irvantaufik28@gmail.com>
2023-03-25 00:07:26 +0700

    fix detail accordion

--- frontend/src/components/cardetail/carDetail.js
+++ frontend/src/components/cardetail/carDetail.js
@@ -1,4 +1,6 @@
 import React from "react";
+// import { LazyLoadImage } from 'react-lazy-load-image-component';
+import Accordion from "react-bootstrap/Accordion";
 import "./detailcar.css";
 
 export default function carDetail(props) {
@@ -26,56 +28,65 @@ export default function carDetail(props) {
               </li>
               <li>Tidak termasuk akomodasi penginapan</li>
             </ul>
-            <h6>Refund, Reschedule, Overtime</h6>
-            <ul>
-              <li>Tidak termasuk biaya makan sopir Rp 75.000/hari</li>
-              <li>
-                Jika overtime lebih dari 12 jam akan ada tambahan biaya Rp
-                20.000/jam
-              </li>
-              <li>Tidak termasuk akomodasi penginapan</li>
-              <li>Tidak termasuk biaya makan sopir Rp 75.000/hari</li>
-              <li>
-                Jika overtime lebih dari 12 jam akan ada tambahan biaya Rp
-                20.000/jam
-              </li>
-              <li>Tidak termasuk akomodasi penginapan</li>
-              <li>Tidak termasuk biaya makan sopir Rp 75.000/hari</li>
-              <li>
-                Jika overtime lebih dari 12 jam akan ada tambahan biaya Rp
-                20.000/jam
-              </li>
-              <li>Tidak termasuk akomodasi penginapan</li>
-            </ul>
+
+           
+              <Accordion className="accordion-detail" defaultActiveKey="1">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header >
+                    Refund, Reschedule, Overtime
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <ul>
+                      <li>Tidak termasuk biaya makan sopir Rp 75.000/hari</li>
+                      <li>
+                        Jika overtime lebih dari 12 jam akan ada tambahan biaya
+                        Rp 20.000/jam
+                      </li>
+                      <li>Tidak termasuk akomodasi penginapan</li>
+                      <li>Tidak termasuk biaya makan sopir Rp 75.000/hari</li>
+                      <li>
+                        Jika overtime lebih dari 12 jam akan ada tambahan biaya
+                        Rp 20.000/jam
+                      </li>
+                      <li>Tidak termasuk akomodasi penginapan</li>
+                      <li>Tidak termasuk biaya makan sopir Rp 75.000/hari</li>
+                      <li>
+                        Jika overtime lebih dari 12 jam akan ada tambahan biaya
+                        Rp 20.000/jam
+                      </li>
+                      <li>Tidak termasuk akomodasi penginapan</li>
+                    </ul>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            
           </div>
         </div>
         <div className="col-md-5 d-flex mt-5 detail-car-card">
           <div className="card-car">
             <div className="image-car-detail">
-              <img
-              
-                src={props.data.image}
-                alt={props.data.name}
-              />
+              <img src={props.data.image} alt={props.data.name} />
 
               <p className="card-title-detail">{props.data.name}</p>
-              <p className="card-title-detail"> {
-                props.data.category === 'small' ? '2-4 orang' :
-                  props.data.category === 'medium' ? '4-6 orang' :
-                    props.data.category === 'large' ? '6-8 orang' :
-                      props.data.category
-              }</p>
+              <p className="card-title-detail">
+                {" "}
+                {props.data.category === "small"
+                  ? "2-4 orang"
+                  : props.data.category === "medium"
+                  ? "4-6 orang"
+                  : props.data.category === "large"
+                  ? "6-8 orang"
+                  : props.data.category}
+              </p>
 
               <div className="row align-items-start">
                 <div className="col">Total</div>
                 <div className="col">{props.data.price}</div>
               </div>
             </div>
-
           </div>
         </div>
       </div>
     </div>
-
   );
 }
