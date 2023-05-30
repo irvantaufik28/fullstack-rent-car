7d1fefb1170d74e458a72285e3bafe993c152d5b
irvantaufik28 <irvantaufik28@gmail.com>
2023-05-30 23:49:34 +0700

    port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,14 +8,17 @@ async function bootstrap() {
   app.enableCors();
 
   const config: ConfigService = app.get(ConfigService);
-  const port: number = config.get<number>('PORT');
-  const host: number = config.get<any>('HOST');
+  // const port: number = config.get<number>('PORT');
+  // const host: number = config.get<any>('HOST');
+  const port: number = 3000;
+  const host: string = '0.0.0.0';
+  
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  await app.listen(port, () => {
-    console.log(`Server listening on http://${host}:${port}`);
-  });
+    await app.listen(port, () => {
+      console.log(`Server listening on http://${host}:${port}`);
+    });
 
   app.enableVersioning({
     type: VersioningType.URI,
