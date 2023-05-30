8cadeca9948365a83b60adb989bea764798e1a3f
irvantaufik28 <irvantaufik28@gmail.com>
2023-05-31 00:16:01 +0700

    port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,24 +2,29 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify';
+import { FastifyRequest, FastifyReply } from 'fastify';
+
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   app.enableCors();
 
   const config: ConfigService = app.get(ConfigService);
-  // const port: number = config.get<number>('PORT');
-  // const host: number = config.get<any>('HOST');
-  const port: number = 4021;
-  const host: string = '0.0.0.0';
-  
+  const port: number = parseInt(config.get<string>('PORT'), 10) || 4021;
+  const host: string = config.get<string>('HOST') || '0.0.0.0';
 
-    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-
-    await app.listen(4021, '0.0.0.0')
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   app.enableVersioning({
     type: VersioningType.URI,
+  });
+
+  await app.listen(port, host, (err, address) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`Server is running on ${address}`);
   });
 }
 
