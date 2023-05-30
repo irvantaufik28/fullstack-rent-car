4b71d4d8d3196dd35d8c2c0d02b5a8b2f1cce532
irvantaufik28 <irvantaufik28@gmail.com>
2023-05-31 00:23:04 +0700

    port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,29 +2,23 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify';
-import { FastifyRequest, FastifyReply } from 'fastify';
-
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule);
   app.enableCors();
 
   const config: ConfigService = app.get(ConfigService);
-  const port: number = parseInt(config.get<string>('PORT'), 10) || 4021;
-  const host: string = config.get<string>('HOST') || '0.0.0.0';
+  const port: number = config.get<number>('PORT');
+  const host: number = config.get<any>('HOST');
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  app.enableVersioning({
-    type: VersioningType.URI,
+  await app.listen(port, () => {
+    console.log(`Server listening on http://${host}:${port}`);
   });
 
-  await app.listen(port, host, (err, address) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(`Server is running on ${address}`);
+  app.enableVersioning({
+    type: VersioningType.URI,
   });
 }
 
