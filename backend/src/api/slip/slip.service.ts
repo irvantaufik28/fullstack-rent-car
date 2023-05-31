873e6118f743bf542593781525550785a6442ece
irvantaufik28 <irvantaufik28@gmail.com>
2023-05-31 21:16:34 +0700

    validate file not null

--- backend/src/api/slip/slip.service.ts
+++ backend/src/api/slip/slip.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SlipEntity } from 'src/database/entities/slip.entity';
 import { Repository } from 'typeorm';
@@ -7,21 +7,23 @@ import { CloudinaryService } from 'src/common/cloudinary/cloudinary.service';
 
 @Injectable()
 export class SlipService {
-    constructor(
-        private readonly cloudinaryService: CloudinaryService,
-        @InjectRepository(SlipEntity)
-        private readonly slipRepository: Repository<SlipEntity>
-        ) {}
+  constructor(
+    private readonly cloudinaryService: CloudinaryService,
+    @InjectRepository(SlipEntity)
+    private readonly slipRepository: Repository<SlipEntity>,
+  ) {}
 
-
-        async createSlip(
-            createSlipDto: CreateSlipDto, user_id : number, file: any, 
-        ) : Promise<CreateSlipDto> {
-            if (file) {
-                const imageUrl = await this.cloudinaryService.uploadImage(file.buffer)
-            createSlipDto.url_slip = imageUrl.url
-            createSlipDto.user_id = user_id
-            }
-            return await this.slipRepository.save(createSlipDto)
-        }
+  async createSlip(
+    createSlipDto: CreateSlipDto,
+    user_id: number,
+    file: any,
+  ): Promise<CreateSlipDto> {
+    if (!file) {
+        throw new HttpException('please insert file', HttpStatus.BAD_REQUEST)
+    }
+    const imageUrl = await this.cloudinaryService.uploadImage(file.buffer);
+    createSlipDto.url_slip = imageUrl.url;
+    createSlipDto.user_id = user_id;
+    return await this.slipRepository.save(createSlipDto);
+  }
 }
